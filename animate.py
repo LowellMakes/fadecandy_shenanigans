dc12d48fb6cc7874c532dcbc09a81112c2f195b7
Patrick Shields <pataelmo@gmail.com>
2015-11-06 23:54:24 -0500

    Capturing simple updates done by others

--- animate.py
+++ animate.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from fc_lib import *
 import opc
 import time
--- pi_cam.py
+++ pi_cam.py
@@ -1,10 +1,30 @@
+#!/usr/bin/python
 from fc_lib import *
 import opc
 import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import os
+import sys
+import signal
+
+pid = str(os.getpid())
+pidfile = "/var/run/fadecandy.pid"
+
+if os.path.isfile(pidfile):
+    print "%s already exists, exiting" % pidfile
+    sys.exit()
+else:
+    file(pidfile, 'w').write(pid)
+
+def set_exit_handler(func):
+    signal.signal(signal.SIGTERM, func)
+def on_exit(sig, func=None):
+    print "exit handler triggered"
+    sys.exit(1)
 
 if __name__ == '__main__':
+    set_exit_handler(on_exit)
     client = opc.Client('localhost:7890')
     camera = PiCamera()
     camera.resolution = (640,480)
